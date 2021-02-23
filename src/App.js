2a0c51b48ecb97f219bd6f15a867a612502b4009
APURBA ROY <apurbaanikroy@gmail.com>
2021-02-23 15:39:03 +0600

    ...

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
   var person = {
@@ -14,12 +15,15 @@ function App() {
     color : 'red',
     backgroundColor : 'yellow'
   }
+  const nayoks = ['Razzak', 'Jasim', 'Alomgir', 'Salman','Shuvo']
   const products = [
     {name:'Photoshop',Price:'$90.99'},
     {name:'Illastrator',Price:'$100.99'},
+    {name: 'Premiar El', Price: '$240'}
 
 ]
-
+const productNames = products.map(product => product.name)
+console.log(productNames);
   return (
     <div className="App">
       <header className="App-header">
@@ -29,16 +33,71 @@ function App() {
         </p>
         <h1 className= "" style = {style}>My heading : {person.name+ " " + person.job}</h1>
         <p>My first react paragraph</p>
+        <ul>
+          {
+            nayoks.map(nayok => <li>{nayok}</li>)
+          }
+          {
+            products.map(product => <li>{product.name}</li>)
+          }
+          <li>{products[0].name}</li>
+        </ul>
+        {
+          products.map(pd =><Products product={pd}></Products>)
+        }
         <h3 style={{backgroundColor:'cyan', color:'yellow'}}>Lala {person2.name +" " + person2.job} </h3>
-        <Products name={products[0].name} Price={products[0].Price}></Products>
-        <Products name={products[1].name}></Products>
+        <Products product = {products[0]}></Products>
+        <Products product= {products[1]}></Products>
         <Person name='Rubel'></Person>
         <Person name='Rahim'></Person>
+        <Counter></Counter>
+        <Users></Users>
       </header>
     </div>
   );
 
 }
+function Counter(){
+  const [count, setCount] =useState(10);
+  const handelDeprese = () =>{
+    const newCount = count-1;
+    setCount(newCount);
+  }
+  const handerIncrease = () =>{
+    const newCount = count +1;
+    setCount(newCount);
+  };
+  return (
+    <div>
+      <h1>Count : {count}</h1>
+      <button onClick={handelDeprese}>Decrease</button>
+      <button onClick={handerIncrease}>Increase</button>
+    </div>
+  )
+}
+function Users(){
+  const [users,setUsers] = useState([]);
+
+  useEffect(()=>{
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then(data => setUsers(data));
+  },[])
+  return (
+    <div>
+      <h3>Dynamic Users :{users.length}</h3>
+      <ul>
+        {
+          users.map(user => <li>{user.email}</li>)
+        }
+        {/* {
+          users.map(user => <li>{user.email}</li>)
+        } */}
+      </ul>
+    </div>
+  )
+}
+
 function Products(props){
   const productStyle={
     border:'1px solid gray',
@@ -48,6 +107,7 @@ function Products(props){
     width: '200px',
     float:'left'
   }
+  const {name,price}=props.product;
   return(
     <div style={productStyle}>
       <h2>{props.name}</h2>
